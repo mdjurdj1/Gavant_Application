131d7da0d41cfdc3328df36c1dbfb7fc3afb81d0
Mdjurdj1 <mdjurdj1@gmail.com>
2017-05-26 09:34:45 -0400

    Added about me section and image

--- app/routes/links.js
+++ app/routes/links.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return ['About Me', 'Resume', 'LinkedIn', 'Github', 'Personal Blog']
+    return ['Resume', 'LinkedIn', 'Github', 'Blog']
   }
 });
